4c2629f8b039e14d50d18ddc272e435890a5e879
Avalon <vilian3737@gmail.com>
2024-01-15 20:16:58 +0200

    Added annotations

--- src/main/java/com/app/booksrestapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/app/booksrestapi/service/impl/BookServiceImpl.java
@@ -1,6 +1,8 @@
 package com.app.booksrestapi.service.impl;
 
 import com.app.booksrestapi.service.BookService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
 }
