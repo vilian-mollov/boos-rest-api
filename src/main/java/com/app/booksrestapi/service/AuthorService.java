b9f57e0737d54c2a5b97e1dfc5d67ad934959dc3
Avalon <vilian3737@gmail.com>
2024-02-02 23:49:11 +0200

    Added Author service method for receiving all authors

--- src/main/java/com/app/booksrestapi/service/AuthorService.java
+++ src/main/java/com/app/booksrestapi/service/AuthorService.java
@@ -1,4 +1,10 @@
 package com.app.booksrestapi.service;
 
+import com.app.booksrestapi.model.entity.Author;
+
+import java.util.List;
+
 public interface AuthorService {
+
+    public List<Author> getAllAuthors();
 }
--- src/main/java/com/app/booksrestapi/web/controller/AuthorController.java
+++ src/main/java/com/app/booksrestapi/web/controller/AuthorController.java
@@ -1,14 +1,29 @@
 package com.app.booksrestapi.web.controller;
 
+
+import com.app.booksrestapi.model.entity.Author;
+import com.app.booksrestapi.service.AuthorService;
+import com.app.booksrestapi.service.impl.AuthorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
+    AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
     @GetMapping
-    public void getAllAuthors() {
-//        TODO get all authors
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
     }
 
     @GetMapping("/{id}")
